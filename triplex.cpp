f61f6986b1ed77d77ec4a32731b72ddf119da7d6
Mar5Planet <amustaf@email.arizona.edu>
2021-01-27 13:27:25 -0700

    triple x game complete

--- triplex.cpp
+++ triplex.cpp
@@ -1,27 +1,72 @@
-//This is a c++ game.
+//This is a c++ CLI game.
 #include <iostream>
+#include <ctime>
 //preprocessor directive
+
+void printIntroduction(int Difficulty)
+{
+    std::cout << "You are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure server room...\nEnter the correct Code to continue..." << "\n";
+    //expression statements
+}
+
+bool playGame(int Difficulty) 
+//Void functions will not return any values.
+{
+
+    printIntroduction(Difficulty);
+    const int CodeA = rand() % (Difficulty + Difficulty);
+    int CodeB = rand() % (Difficulty + Difficulty);
+    int CodeC = rand() % (Difficulty + Difficulty);
+    //declaration statements
+
+    int CodeSum = CodeA+CodeB+CodeC;
+    int CodeProduct = CodeA*CodeB*CodeC;
+
+    std::cout << "There are 3 numbers in the Code" << "\n";
+    std::cout << "The Codes add up to " << CodeSum << "\n";
+    std::cout << "The Codes multiply to give " << CodeProduct << "\n";
+    
+    std::cout << "Enter the secret Code: ";
+    
+    //Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+  
+    //check if player Guess is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) 
+    {
+        std::cout << "Correct!\n";
+        return true;
+    } else {
+        std::cout << "The answer is incorrect, try again!" << "\n";
+        return false;
+    }
+}
 
 
 int main() //main function
 {
-    std::cout << "You are a secret agent breaking into a secure server room..." << "\n";
-    std::cout << "Enter the correct code to continue..." << "\n";
-    //expression statements
+    srand(time(NULL)); //seeding rand number based on time of day.
+    int LevelDifficulty = 1;
+    int MaxDifficulty = 5;
+    while (LevelDifficulty <= MaxDifficulty)
+    {
+        bool bLevelComplete = playGame(LevelDifficulty);
+        std::cin.clear(); //clears any errors
+        std::cin.ignore(); //discards the buffer
 
-    const int a = 4;
-    int b = 20;
-    int c = 69;
-    //declaration statements
-
-    b = 7;
-
-    int sum = a+b+c;
-    int product = a*b*c;
-
-    std::cout << product << "\n";
-
+        if (bLevelComplete) {
+            ++LevelDifficulty;
+            //increase level difficulty
+        } 
+    }
     
     return 0;
     //return statement
 }
+
