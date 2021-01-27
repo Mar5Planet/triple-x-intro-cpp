78a26e0eb3f8d82861aeaf79bf8963f7ef2c5ec9
Mar5Planet <amustaf@email.arizona.edu>
2021-01-27 10:58:39 -0700

    updated triplex file

---
+++ triplex.cpp
@@ -0,0 +1,27 @@
+//This is a c++ game.
+#include <iostream>
+//preprocessor directive
+
+
+int main() //main function
+{
+    std::cout << "You are a secret agent breaking into a secure server room..." << "\n";
+    std::cout << "Enter the correct code to continue..." << "\n";
+    //expression statements
+
+    const int a = 4;
+    int b = 20;
+    int c = 69;
+    //declaration statements
+
+    b = 7;
+
+    int sum = a+b+c;
+    int product = a*b*c;
+
+    std::cout << product << "\n";
+
+    
+    return 0;
+    //return statement
+}
